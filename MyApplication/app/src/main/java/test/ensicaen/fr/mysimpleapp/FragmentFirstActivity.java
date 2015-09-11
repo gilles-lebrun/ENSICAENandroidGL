6ac9312fe039234c8af08ea8038064b0034cbf72
Gilles Lebrun Perso <gilles.lebrun.caen@gmail.com>
2015-09-11 17:38:43 +0200

    Prise en compte des 4 types d'animations pour le FragmentFirstActivityFragment

--- MyApplication/app/src/main/java/test/ensicaen/fr/mysimpleapp/FragmentFirstActivity.java
+++ MyApplication/app/src/main/java/test/ensicaen/fr/mysimpleapp/FragmentFirstActivity.java
@@ -1,16 +1,37 @@
 package test.ensicaen.fr.mysimpleapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.AutoTransition;
+import android.transition.Explode;
+import android.transition.Fade;
+import android.transition.Slide;
+import android.transition.TransitionManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class FragmentFirstActivity extends AppCompatActivity {
+    private ViewGroup viewGroup;
+    private TextView textView;
+    private ImageView imageView1, imageView2, imageView3;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_first);
+        viewGroup = (ViewGroup) findViewById(R.id.fragment_first_activity);
+        if (viewGroup==null) Log.d("ANIMATION","viewGroup is null !!!");
+        textView = (TextView) findViewById(R.id.textViewAnimation);
+        imageView1 = (ImageView) findViewById(R.id.iconSun1);
+        imageView2 = (ImageView) findViewById(R.id.iconSun2);
+        imageView3 = (ImageView) findViewById(R.id.iconSun3);
+        count = 0;
     }
 
 
@@ -34,5 +55,43 @@ public class FragmentFirstActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void launchAnim(View view) {
+        Log.v("ANIMATION", "effect : Explode, Fade or Slide");
+        // voir : https://developer.android.com/reference/android/transition/Transition.html
+        switch (count/2){
+            case 0:
+                TransitionManager.beginDelayedTransition(viewGroup, new Explode());
+                break;
+            case 1:
+                TransitionManager.beginDelayedTransition(viewGroup, new Fade());
+                break;
+            case 2:
+                TransitionManager.beginDelayedTransition(viewGroup, new Slide());
+                break;
+            case 3:
+                TransitionManager.beginDelayedTransition(viewGroup, new AutoTransition());
+                break;
+            default:
+                Log.d("ANIMATION", "Erreur de valeur pour le switch");
+
+        }
+
+        Log.v("ANIMATION","toggle");
+        toggle(textView, imageView1, imageView2, imageView3);
+        count+=1;
+        count%=8;
+
+    }
+
+    private static void toggle(View... views){
+        for (View v:views
+             ) {
+            boolean isVisible = v.getVisibility() == View.VISIBLE;
+            v.setVisibility(isVisible ? View.INVISIBLE : View.VISIBLE);
+
+        }
+
     }
 }
